7b00ac449e9e58cb22c7ed5bfc4bd88c9f4f5994
TitleHHHH <TitleHHHH@outlook.com>
2022-12-13 19:26:24 +0500

    234

--- McSpammer/Program.cs
+++ McSpammer/Program.cs
@@ -8,7 +8,7 @@ namespace McSpammer
     internal class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // SynchronizationContext-reliant code before AppMain is called: things arden't initialized
         // yet and stuff might break.
         [STAThread]
         public static void Main(string[] args) => BuildAvaloniaApp()
