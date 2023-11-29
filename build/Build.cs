ac35d90481012c34e7fc90fd9856997a94ee5f74
TitleHHHH <TitleHHHH@outlook.com>
2023-11-29 22:05:45 +0500

    add cache artifacts

--- build/Build.cs
+++ build/Build.cs
@@ -245,26 +245,24 @@ class Build : NukeBuild
 	   .Requires(() => Configuration.Equals(Configuration.Release))
 	   .Executes(async () =>
 	   {
-		   Console.WriteLine("asdasd");
+
+		   foreach (var file in ArtifactsDirectory.GetFiles())
+		   {
+			   Console.WriteLine(file);
+		   }
+
 		   var credentials = new Credentials(GitHubActions.Token);
 
 		   var gitHubClient = new GitHubClient(new ProductHeaderValue(nameof(NukeBuild)),
 				new InMemoryCredentialStore(credentials));
 
-		   Console.WriteLine("asdasd");
 
 
 
 		   var (owner, name) = (GitRepository.GetGitHubOwner(), GitRepository.GetGitHubName());
-		   Console.WriteLine("asdasd");
+
 		   var response = await gitHubClient.Actions.Artifacts.ListWorkflowArtifacts(owner, name, GitHubActions.RunId);
-		   GitHubActions.WriteDebug("List artifacts:");
-		   Console.WriteLine("asdasd");
-		   foreach (var artifact in response.Artifacts)
-		   {
-			   GitHubActions.WriteDebug(artifact.Name);
-		   }
-		   Console.WriteLine("asdasd");
+
 		   GitHubActions.WriteDebug("List End:");
 		   return;
 		   var releaseTag = MinVer.Version;
