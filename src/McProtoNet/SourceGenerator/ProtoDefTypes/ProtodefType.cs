d2b8d7587e1b2375ac0d24e7653a4d2e68aa3773
TitleHHHH <TitleHHHH@outlook.com>
2024-03-23 22:56:04 +0500

    add parent

--- src/McProtoNet/SourceGenerator/ProtoDefTypes/ProtodefType.cs
+++ src/McProtoNet/SourceGenerator/ProtoDefTypes/ProtodefType.cs
@@ -4,9 +4,20 @@ using System.Text.Json.Serialization;
 namespace SourceGenerator.ProtoDefTypes
 {
 	[JsonConverter(typeof(DataTypeConverter))]
-	public abstract class ProtodefType
+	public abstract class ProtodefType: IJsonOnDeserialized
 	{
 		public ProtodefType? Parent { get; set; }
+
+		public void OnDeserialized()
+		{
+			if(this is IPathTypeEnumerable enums)
+			{
+				foreach(var item in enums)
+				{
+					item.Value.Parent = this;
+				}
+			}
+		}
 	}
 
 
