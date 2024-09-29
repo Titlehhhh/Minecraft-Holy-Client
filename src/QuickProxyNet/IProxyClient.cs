dd800fc47589174ff94c5ab3d3f5dfe54bb10ba8
TitleHHHH <TitleHHHH@outlook.com>
2024-09-29 15:13:48 +0500

    add CancellationToken. Remove async filed

--- src/QuickProxyNet/IProxyClient.cs
+++ src/QuickProxyNet/IProxyClient.cs
@@ -2,57 +2,6 @@
 
 namespace QuickProxyNet;
 
-internal static class ProxyConnector
-{
-    public static async ValueTask<Stream> ConnectToProxyAsync(Stream stream, Uri proxyUri, string host, int port,
-        NetworkCredential? proxyCredentials, CancellationToken cancellationToken)
-    {
-        using (cancellationToken.Register(s => ((Stream)s!).Dispose(), stream))
-        {
-            try
-            {
-                var credentials = proxyCredentials?.GetCredential(proxyUri, proxyUri.Scheme);
-
-                if (string.Equals(proxyUri.Scheme, "socks5", StringComparison.OrdinalIgnoreCase))
-                {
-                    await SocksHelper.EstablishSocks5TunnelAsync(stream, host, port, credentials, true)
-                        .ConfigureAwait(false);
-                    return stream;
-                }
-
-                if (string.Equals(proxyUri.Scheme, "socks4a", StringComparison.OrdinalIgnoreCase))
-                {
-                    await SocksHelper.EstablishSocks4TunnelAsync(stream, true, host, port, credentials, true)
-                        .ConfigureAwait(false);
-                    return stream;
-                }
-
-                if (string.Equals(proxyUri.Scheme, "socks4", StringComparison.OrdinalIgnoreCase))
-                {
-                    await SocksHelper.EstablishSocks4TunnelAsync(stream, false, host, port, credentials, true)
-                        .ConfigureAwait(false);
-                    return stream;
-                }
-
-                if (string.Equals(proxyUri.Scheme, "http", StringComparison.OrdinalIgnoreCase) ||
-                    string.Equals(proxyUri.Scheme, "https", StringComparison.OrdinalIgnoreCase))
-                {
-                    var result = await HttpHelper.EstablishHttpTunnelAsync(stream, proxyUri, host, port, credentials,
-                        cancellationToken);
-                    return result;
-                }
-
-                throw new NotSupportedException("Bad protocol");
-            }
-            catch (Exception ex)
-            {
-                stream.Dispose();
-                throw;
-            }
-        }
-    }
-}
-
 public interface IProxyClient
 {
     NetworkCredential? ProxyCredentials { get; }
---
+++ src/QuickProxyNet/ProxyConnector.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+namespace QuickProxyNet;
+
+internal static class ProxyConnector
+{
+    public static async ValueTask<Stream> ConnectToProxyAsync(Stream stream, Uri proxyUri, string host, int port,
+        NetworkCredential? proxyCredentials, CancellationToken cancellationToken)
+    {
+        using (cancellationToken.Register(s => ((Stream)s!).Dispose(), stream))
+        {
+            try
+            {
+                var credentials = proxyCredentials?.GetCredential(proxyUri, proxyUri.Scheme);
+
+                if (string.Equals(proxyUri.Scheme, "socks5", StringComparison.OrdinalIgnoreCase))
+                {
+                    await SocksHelper.EstablishSocks5TunnelAsync(stream, host, port, credentials, cancellationToken)
+                        .ConfigureAwait(false);
+                    return stream;
+                }
+
+                if (string.Equals(proxyUri.Scheme, "socks4a", StringComparison.OrdinalIgnoreCase))
+                {
+                    await SocksHelper.EstablishSocks4TunnelAsync(stream, true, host, port, credentials, cancellationToken)
+                        .ConfigureAwait(false);
+                    return stream;
+                }
+
+                if (string.Equals(proxyUri.Scheme, "socks4", StringComparison.OrdinalIgnoreCase))
+                {
+                    await SocksHelper.EstablishSocks4TunnelAsync(stream, false, host, port, credentials, cancellationToken)
+                        .ConfigureAwait(false);
+                    return stream;
+                }
+
+                if (string.Equals(proxyUri.Scheme, "http", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(proxyUri.Scheme, "https", StringComparison.OrdinalIgnoreCase))
+                {
+                    var result = await HttpHelper.EstablishHttpTunnelAsync(stream, proxyUri, host, port, credentials,
+                        cancellationToken);
+                    return result;
+                }
+
+                throw new NotSupportedException("Bad protocol");
+            }
+            catch (Exception ex)
+            {
+                stream.Dispose();
+                throw;
+            }
+        }
+    }
+}
