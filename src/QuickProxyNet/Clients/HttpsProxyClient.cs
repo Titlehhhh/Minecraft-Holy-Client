a42ce7d5d50c277b4c27d14680e9d93abfd3733b
TitleHHHH <TitleHHHH@outlook.com>
2024-10-13 19:03:50 +0500

    add https proxy

--- src/QuickProxyNet/Clients/HttpsProxyClient.cs
+++ src/QuickProxyNet/Clients/HttpsProxyClient.cs
@@ -1,10 +1,15 @@
 ﻿using System.Net;
+using System.Net.Security;
 using System.Runtime.CompilerServices;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 namespace QuickProxyNet;
 
 public class HttpsProxyClient : ProxyClient
 {
+    private SslCertificateValidationInfo sslValidationInfo;
+
     public HttpsProxyClient(string host, int port) : base("https", host, port)
     {
     }
@@ -14,15 +19,130 @@ public class HttpsProxyClient : ProxyClient
     {
     }
 
+    public RemoteCertificateValidationCallback ServerCertificateValidationCallback { get; set; }
+
+    public bool CheckCertificateRevocation { get; set; }
+
+    public X509CertificateCollection ClientCertificates { get; set; }
+
+    public CipherSuitesPolicy SslCipherSuitesPolicy { get; set; }
+
+    public SslProtocols SslProtocols { get; set; } = SslProtocols.Tls12 | SslProtocols.Tls13;
+
     public override ProxyType Type => ProxyType.HTTPS;
+
+    private SslClientAuthenticationOptions GetSslClientAuthenticationOptions(string host,
+        RemoteCertificateValidationCallback remoteCertificateValidationCallback)
+    {
+        return new SslClientAuthenticationOptions
+        {
+            CertificateRevocationCheckMode =
+                CheckCertificateRevocation ? X509RevocationMode.Online : X509RevocationMode.NoCheck,
+            ApplicationProtocols = new List<SslApplicationProtocol> { SslApplicationProtocol.Http11 },
+            RemoteCertificateValidationCallback = remoteCertificateValidationCallback,
+
+            CipherSuitesPolicy = SslCipherSuitesPolicy,
+
+            ClientCertificates = ClientCertificates,
+            EnabledSslProtocols = SslProtocols,
+            TargetHost = host
+        };
+    }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public override async ValueTask<Stream> ConnectAsync(Stream stream, string host, int port,
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var ssl = new SslStream(stream,false, ValidateRemoteCertificate);
+        try
+        {
+            //await Task.Delay(100);
+            //await ssl.AuthenticateAsClientAsync(host);
+            
+           // if(false)
+            await ssl.AuthenticateAsClientAsync(GetSslClientAuthenticationOptions(host, ValidateRemoteCertificate),
+                cancellationToken);
+        }
+        catch (Exception e)
+        {
+            ssl.Dispose();
+            throw;
+        }
+
         var result =
-            await ProxyConnector.ConnectToProxyAsync(stream, ProxyUri, host, port, ProxyCredentials, cancellationToken);
+            await HttpHelper.EstablishHttpTunnelAsync(ssl, ProxyUri, host, port, ProxyCredentials, cancellationToken);
         return result;
+    }
+
+    private bool ValidateRemoteCertificate(object sender, X509Certificate certificate, X509Chain chain,
+        SslPolicyErrors sslPolicyErrors)
+    {
+        bool valid;
+
+        sslValidationInfo?.Dispose();
+        sslValidationInfo = null;
+
+        if (ServerCertificateValidationCallback != null)
+            valid = ServerCertificateValidationCallback(ProxyHost, certificate, chain, sslPolicyErrors);
+        else if (ServicePointManager.ServerCertificateValidationCallback != null)
+            valid = ServicePointManager.ServerCertificateValidationCallback(ProxyHost, certificate, chain,
+                sslPolicyErrors);
+        else
+            valid = sslPolicyErrors == SslPolicyErrors.None;
+
+        if (!valid)
+            // Note: The SslHandshakeException.Create() method will nullify this once it's done using it.
+            sslValidationInfo = new SslCertificateValidationInfo(sender, certificate, chain, sslPolicyErrors);
+
+        return valid;
+    }
+
+    private sealed class SslChainElement : IDisposable
+    {
+        public readonly X509Certificate2 Certificate;
+        public readonly X509ChainStatus[] ChainElementStatus;
+        public readonly string Information;
+
+        public SslChainElement(X509ChainElement element)
+        {
+            Certificate = new X509Certificate2(element.Certificate.RawData);
+            ChainElementStatus = element.ChainElementStatus;
+            Information = element.Information;
+        }
+
+        public void Dispose()
+        {
+            Certificate.Dispose();
+        }
+    }
+
+    private sealed class SslCertificateValidationInfo : IDisposable
+    {
+        public readonly X509Certificate2 Certificate;
+        public readonly List<SslChainElement> ChainElements;
+        public readonly X509ChainStatus[] ChainStatus;
+        public readonly string Host;
+        public readonly SslPolicyErrors SslPolicyErrors;
+
+        public SslCertificateValidationInfo(object sender, X509Certificate certificate, X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            Certificate = new X509Certificate2(certificate.Export(X509ContentType.Cert));
+            ChainElements = new List<SslChainElement>();
+            SslPolicyErrors = sslPolicyErrors;
+            ChainStatus = chain.ChainStatus;
+            Host = sender as string;
+
+            // Note: we need to copy the ChainElements because the chain will be destroyed
+            foreach (var element in chain.ChainElements)
+                ChainElements.Add(new SslChainElement(element));
+        }
+
+        public void Dispose()
+        {
+            Certificate.Dispose();
+            foreach (var element in ChainElements)
+                element.Dispose();
+        }
     }
 }
---
+++ src/QuickProxyNet/Internal/HttpResponseParser.cs
@@ -0,0 +1,98 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using DotNext.Buffers;
+
+namespace QuickProxyNet;
+
+internal struct HttpResponseParser
+{
+    private static int BufferSize = 1024;
+    private MemoryOwner<byte> _memory;
+    private static readonly MemoryAllocator<byte> _allocator = ArrayPool<byte>.Shared.ToAllocator();
+    private static readonly byte[] http200 = "HTTP/1.1 200".Select(x => (byte)x).ToArray();
+    private int _writtenCount = 0;
+    private int _indexEnd=-1;
+    public ReadOnlySpan<byte> Span => _memory.Span.Slice(0, _writtenCount);
+
+    public HttpResponseParser()
+    {
+        _memory = _allocator.AllocateExactly(BufferSize);
+    }
+
+    public Memory<byte> GetMemory()
+    {
+        if (_writtenCount < _memory.Length)
+        {
+            return _memory.Memory.Slice(_writtenCount);
+        }
+
+        _memory.Resize(_writtenCount + BufferSize);
+        return _memory.Memory.Slice(_writtenCount);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Parse(int count)
+    {
+        _writtenCount += count;
+        return ParseHttpEnd(count);
+    }
+
+    private static readonly byte[] NewLine = "\r\n\r\n".Select(x => (byte)x).ToArray();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool ParseHttpEnd(int count)
+    {
+        int start = _writtenCount - count;
+        int length = count;
+
+        int offset = Math.Min(4, start);
+        start -= offset;
+        length += offset;
+
+        ReadOnlySpan<byte> bytes = _memory.Span.Slice(start, length);
+
+        int index = bytes.IndexOf(NewLine);
+        if (index < 0)
+        {
+            return false;
+        }
+
+        _indexEnd = index + start;
+
+        return true;
+    }
+
+    public bool Validate()
+    {
+        if (_indexEnd == -1)
+        {
+            throw new InvalidOperationException("No find http response");
+        }
+        
+        ReadOnlySpan<byte> span = Span;
+        
+        
+        if (span.Length > (uint)_indexEnd + 4)
+        {
+            return false;
+        }
+        if (span.Length >= 15 && span.StartsWith(http200))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString()
+    {
+        return Encoding.UTF8.GetString(_memory.Span.Slice(0, _indexEnd+4));
+    }
+
+    public void Dispose()
+    {
+        _memory.Dispose();
+    }
+}
